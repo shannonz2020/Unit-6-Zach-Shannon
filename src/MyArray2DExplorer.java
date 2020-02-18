22b10ab2498f488897bcd863e6688c89f0e7593b
shannonz2020 <54951287+shannonz2020@users.noreply.github.com>
2020-02-18 09:54:36 -0800

    updated some stuff

--- src/MyArray2DExplorer.java
+++ src/MyArray2DExplorer.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+
 public class MyArray2DExplorer {
 
     public static void main(String[] args) {
         int[][] array = {  {4,1,8,5},
                             {0,2,3,4},
                             {6,6,2,2} };
+        int[][] array2 = {  {1,2,3,4},
+                            {5,6,7,8},
+                            {9,10,11,12} };
         MyArray2DExplorer exp = new MyArray2DExplorer();
 
-        System.out.println("Test evenRow: \n Row w/ odd, Expecting: false \n Actual: " + exp.evenRow(array,0));
-        System.out.println("Test evenRow: \n Row w/o odd, Expecting: true \n Actual: " + exp.evenRow(array,2));
+        //System.out.println("Test evenRow: \n Row w/ odd, Expecting: false \n Actual: " + exp.evenRow(array,0));
+        //System.out.println("Test evenRow: \n Row w/o odd, Expecting: true \n Actual: " + exp.evenRow(array,2));
 
         System.out.println();
 
@@ -17,13 +22,15 @@ public class MyArray2DExplorer {
         System.out.println();
 
 
-        System.out.print("Test minRow: \n Expecting: {0 2 3 4} \n Actual: {");
+        /*System.out.print("Test minRow: \n Expecting: {0 2 3 4} \n Actual: {");
         int [] row = exp.minRow(array);
         for(int i = 0; i < row.length; i++){
             System.out.print(row[i] + " ");
         }
         System.out.print("}");
-
+*/
+        System.out.println("1,2,3,4");
+        System.out.println(Arrays.toString(exp.colMaxs(array2)));
     }
 
     //WRITE YOUR METHODS BELOW
@@ -59,12 +66,18 @@ public class MyArray2DExplorer {
         return minRow;
     }
 
-
-
-
-
-
-
-
+    public int[] colMaxs(int[][] matrix){
+        int min = matrix[0][0];
+        int[] output = new int[matrix[0].length];
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] < min){
+                    min = matrix[i][j];
+                }
+            }
+            output[i] =  min;
+        }
+        return output;
+    }
 
 }
