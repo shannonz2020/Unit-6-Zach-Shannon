c3ccbe4b1752253901489e8b06517e52589a4068
shannonz2020 <54951287+shannonz2020@users.noreply.github.com>
2020-02-20 09:53:59 -0800

    Yeet or something

--- src/MyArray2DExplorer.java
+++ src/MyArray2DExplorer.java
@@ -3,34 +3,46 @@ import java.util.Arrays;
 public class MyArray2DExplorer {
 
     public static void main(String[] args) {
-        int[][] array = {  {4,1,8,5},
+        int array [][] = {  {4,1,8,5},
                             {0,2,3,4},
                             {6,6,2,2} };
-        int[][] array2 = {  {1,2,3,4},
-                            {5,6,7,8},
-                            {9,10,11,12} };
         MyArray2DExplorer exp = new MyArray2DExplorer();
 
-        //System.out.println("Test evenRow: \n Row w/ odd, Expecting: false \n Actual: " + exp.evenRow(array,0));
-        //System.out.println("Test evenRow: \n Row w/o odd, Expecting: true \n Actual: " + exp.evenRow(array,2));
+        System.out.println("Test evenRow: \n Row w/ odd, Expecting: false \n Actual: " + exp.evenRow(array,0));
+        System.out.println("Test evenRow: \n Row w/o odd, Expecting: true \n Actual: " + exp.evenRow(array,2));
 
         System.out.println();
+        System.out.print("Test minRow: \n Expecting: {0 2 3 4} \n Actual: {");
 
-        //System.out.println("Test oddColSum: \n Col w/ odd Sum, Expecting: true \n Actual: " + exp.oddColSum(array,1));
-        //System.out.println("Test oddColSum: \n Col w/ even, Expecting: false \n Actual: " + exp.oddColSum(array,0));
-
-        System.out.println();
-
-
-        /*System.out.print("Test minRow: \n Expecting: {0 2 3 4} \n Actual: {");
-        int [] row = exp.minRow(array);
-        for(int i = 0; i < row.length; i++){
-            System.out.print(row[i] + " ");
+        System.out.print("Test colMax: \n Expecting: {6 6 8 5} \n Actual: {");
+        int [] colMaxs = exp.colMaxs(array);
+        for(int i = 0; i < colMaxs.length; i++){
+            System.out.print(colMaxs[i] + " ");
         }
         System.out.print("}");
-*/
-        System.out.println("1,2,3,4");
-        System.out.println(Arrays.toString(exp.colMaxs(array2)));
+
+        System.out.println();
+        System.out.println();
+
+        System.out.print("Test allRowSum: \n Expecting: {18 9 16} \n Actual: {");
+        int [] sum = exp.allRowSums(array);
+        for(int i = 0; i < sum.length; i++){
+            System.out.print(sum[i] + " ");
+        }
+        System.out.print("}");
+
+        System.out.println();
+
+        System.out.print("Test averageCol: \n Expecting: {3.333333 3.0 4.333333 3.6666667} \n Actual: {");
+        double [] avg = exp.averageCol(array);
+        for(int i = 0; i < avg.length; i++){
+            System.out.print(avg[i] + " ");
+        }
+        System.out.print("}");
+
+
+        System.out.println();
+
     }
 
     //WRITE YOUR METHODS BELOW
@@ -52,20 +64,21 @@ public class MyArray2DExplorer {
     }
 
 
-    public int[] minRow(int [][] mat){
-        int minNum = mat[0][0];
-        int[] minRow = mat[0];
-        for(int i = 0; i < mat.length; i++){
-            for (int e = 0; e < mat[i].length; e++){
-                if(mat[i][e] < minNum){
-                    minNum = mat[i][e];
-                    minRow = mat[i];
+    public int[] minRow(int [][] matrix){
+        int minNum = matrix[0][0];
+        int[] minRow = matrix[0];
+        for(int i = 0; i < matrix.length; i++){
+            for (int e = 0; e < matrix[i].length; e++){
+                if(matrix[i][e] < minNum){
+                    minNum = matrix[i][e];
+                    minRow = matrix[i];
                 }
             }
         }
         return minRow;
     }
 
+    //fix
     public int[] colMaxs(int[][] matrix){
         int min = matrix[0][0];
         int[] output = new int[matrix[0].length];
@@ -79,5 +92,31 @@ public class MyArray2DExplorer {
         }
         return output;
     }
+    public int[] allRowSums(int[][] matrix){
+        int[] output = new int[matrix[0].length -1];
+        int sum = 0;
+        for(int i = 0; i < matrix.length ; i++) {
+            sum = 0;
+            for (int j = 0; j < matrix[0].length ; j++) {
+                sum += matrix[i][j];
+            }
+            output[i] = sum;
+        }
+        return output;
+    }
+
+    //fix
+    public double[] averageCol(int[][] matrix){
+        double[] output = new double[matrix[0].length];
+        double sum = 0;
+        for(int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                sum += matrix[i][j];
+            }
+            output[i] = sum/matrix[0].length;
+        }
+        return output;
+    }
+
 
 }
