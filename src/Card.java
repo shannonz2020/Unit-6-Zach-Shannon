661e7d2f93668f9c8d1b121243ad357d442d538c
shannonz2020 <54951287+shannonz2020@users.noreply.github.com>
2020-02-28 09:27:34 -0800

    Made Card

---
+++ src/Card.java
@@ -0,0 +1,22 @@
+public class Card implements Comparable{
+    private int suit;
+    private int rank;
+
+    public Card(int suit2, int rank2){
+        suit = suit2;
+        rank = rank2;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public int compareTo(Object cardy){
+        if(this.rank > )
+    }
+
+}
